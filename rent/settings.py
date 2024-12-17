9dfc0a6284154dfe15373fe7d46661dd72f3eda4
Drini23 <122325981+Drini23@users.noreply.github.com>
2024-12-17 21:02:15 +0100

    env

--- rent/settings.py
+++ rent/settings.py
@@ -116,9 +116,9 @@ STRIPE_SECRET_KEY_TEST_MODE = config('STRIPE_SECRET_KEY_TEST_MODE')
 
 
 
-EMAIL_BACKEND = config('EMAIL_BACKEND')
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT=587
-EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)  # Cast to bool
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.smtp.EmailBackend')
+EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
+EMAIL_PORT = config('EMAIL_PORT', cast=int, default=587)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool, default=True)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
